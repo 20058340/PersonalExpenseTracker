fb9527e2f4987280a36a09dbeeb99f300ee7557b
20058340 <20058340@mydbs.ie>
2024-12-05 21:44:21 +0530

    added command for deleted budget

--- app.js
+++ app.js
@@ -190,6 +190,7 @@ function updateBudgetStatus() {
 
 }
 
+//delete budget
 function deleteBudget(id) {
     if (confirm("Are you sure you want to delete this budget?")) {
         fetch(`${baseURL}/budgets/${id}`, { method: 'DELETE' })
