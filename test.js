38df300aa803c25ede74ffe1ca68bba371670ccb
20058340 <barath>
2024-12-11 23:56:08 +0530

    function to test categories.

--- test.js
+++ test.js
@@ -5,3 +5,11 @@ jest.mock('jquery', () => ({
     get: jest.fn(),
     ajax: jest.fn(),
 }));
+
+describe('Expense Tracker Application', () => {
+
+    // Test fetching categories
+    it('should fetch categories and display them', async () => {
+        $.get.mockImplementationOnce((url, callback) => {
+            callback({ categories: [{ id: 1, name: 'Food' }, { id: 2, name: 'Entertainment' }] });
+        });
