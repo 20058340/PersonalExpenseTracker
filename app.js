36429cb61bd9c46b95cf404f248b2d27c988d6f5
20058340 <20058340@mydbs.ie>
2024-12-11 08:14:13 +0530

    updated the github link in the url

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    const API_BASE_URL = 'http://localhost:4000';
+    const API_BASE_URL = 'https://20058340.github.io/PersonalExpenseTracker/';
 
     // Initial fetch
     fetchCategories();
